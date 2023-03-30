dd566cbd3ffc7782254f7eb597a274630d6700ee
ankit3r <64691445+ankit3r@users.noreply.github.com>
2023-03-30 14:01:23 +0530

    day night problem solve it

--- app/src/main/java/com/example/mymovies/repository/MovieRepo.kt
+++ app/src/main/java/com/example/mymovies/repository/MovieRepo.kt
@@ -1,11 +1,17 @@
 package com.example.mymovies.repository
 
 import com.example.mymovies.apiService.ApiCall
+import com.example.mymovies.models.movieDetails.MovieData
 import com.example.mymovies.models.movieModel.MovieModel
+import com.example.mymovies.models.tvShowDetails.TvShowData
 import com.example.mymovies.models.tvShowModel.TvShowModel
 import com.example.mymovies.utils.ResponseData
 
-class MovieRepo(private val movieApi: ApiCall, private val apiKey: String) {
+class MovieRepo(
+    private val movieApi: ApiCall,
+    private val apiKey: String,
+    private val baseUrl: String
+) {
     suspend fun getPopularMovies(page: Int = 1): ResponseData<MovieModel> {
         return try {
             val response = movieApi.getPopularMovies(apiKey, page = page)
@@ -63,6 +69,38 @@ class MovieRepo(private val movieApi: ApiCall, private val apiKey: String) {
             }
         } catch (e: Exception) {
             ResponseData.Error("Failed to retrieve popular movies: ${e.message}")
+        }
+    }
+
+    suspend fun getTvShowById(id: Int): ResponseData<TvShowData> {
+        val url = "$baseUrl/tv/$id?api_key=$apiKey&language=en-US"
+        return try {
+            val response = movieApi.getTvShowById(url)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    ResponseData.Success(it)
+                } ?: ResponseData.Error("Response body is null")
+            } else {
+                ResponseData.Error("Failed to retrieve Tv Show: ${response.code()}")
+            }
+        } catch (e: Exception) {
+            ResponseData.Error("Failed to retrieve Tv Show: ${e.message}")
+        }
+    }
+
+    suspend fun getMovieById(id: Int): ResponseData<MovieData> {
+        val url = "$baseUrl/movie/$id?api_key=$apiKey&language=en-US"
+        return try {
+            val response = movieApi.getMovieById(url)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    ResponseData.Success(it)
+                } ?: ResponseData.Error("Response body is null")
+            } else {
+                ResponseData.Error("Failed to retrieve movies Data: ${response.code()}")
+            }
+        } catch (e: Exception) {
+            ResponseData.Error("Failed to retrieve movies Data: ${e.message}")
         }
     }
 
